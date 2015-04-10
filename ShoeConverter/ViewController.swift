8809789a9889ab6ba2fc51637b04e6f27dbf9606
Philip Pang <ohilib@gmail.com>
2015-04-10 21:51:54 +0800

    first commit

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextField.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!
+        let conversionConstant = 30
+        integerFromTextField += conversionConstant
+        mensConvertedShoeSizeLabel.hidden = false
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+    }
 
 }
 
